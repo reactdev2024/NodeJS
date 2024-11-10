619a7897aa8cd795fb7bec53c5929ef601197370
Shaikh Tausif <shaikhtausif71@gmail.com>
2024-11-10 23:57:07 +0530

    First Route in route folder

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express'); //import express package to use express feature
 
+const indexRouter = require('./routes/IndexRoute'); //import index router in app.js file
+
 const app = express();  // in app variable we store reference of expresss package
+
+app.use(indexRouter); // register index router
 
 // app.listen, that starts your server and tells it to "listen" for requests on a specific port (like a door where requests come in).
 app.listen(3000, () => {
---
+++ routes/IndexRoute.js
@@ -0,0 +1,14 @@
+const express = require('express'); // import express
+
+const router = express.Router(); //  use for manage route & create routes
+
+// this will create get route for /index
+// router have http method like get,post,put & delete
+// this will accept route name & callback function with request, response & next parameter
+// we can get paramter name & body or header using req object
+// we can send response using res object like json or web pages
+router.get('/index', (req, res, next) => {
+    res.send('<h1>Hello World..!');
+});
+
+module.exports = router;
