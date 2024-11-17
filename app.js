20bb94a3b3df666b58f55e19d908a47142a4afe6
Shaikh Tausif <shaikhtausif71@gmail.com>
2024-11-17 21:01:02 +0530

    Environment File using dotenv package

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express'); //import express package to use express feat
 const connectDB = require('./util/dbConnection');
 const indexRouter = require('./routes/IndexRoute'); //import index router in app.js file
 
+const dotenv = require('dotenv');
+dotenv.config(); // import .env file to Use environment variable
+
 const app = express();  // in app variable we store reference of expresss package
 
 app.set('view engine', 'ejs'); //register template engine
 app.set('views', 'views');     // also inform express our views default folder is views
 
 app.use(indexRouter); // register index router
-connectDB();
+connectDB(); // this function is use to connecting database
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT_NO || 3000;
 // app.listen, that starts your server and tells it to "listen" for requests on a specific port (like a door where requests come in).
 app.listen(PORT, () => {
     console.log(`server is running on port number ${PORT}`);
--- util/dbConnection.js
+++ util/dbConnection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config(); // import .env file to Use environment variable
 
 const connectDB = async () => {
     // MongoDB connection URL
-    const dbURI = 'mongodb+srv://shaikhtausif71:tausif71@portfoliocluster.qnfrt.mongodb.net/?retryWrites=true&w=majority&appName=PortfolioCluster'; // Replace with your MongoDB URI
+    //const dbURI = 'mongodb+srv://shaikhtausif71:tausif71@portfoliocluster.qnfrt.mongodb.net/?retryWrites=true&w=majority&appName=PortfolioCluster'; // Replace with your MongoDB URI
+    const dbURI = process.env.MONGODB_URI; // Use the MongoDB URI from the .env file
 
     try {
-        await mongoose.connect(dbURI); // Simplified connection
+        await mongoose.connect(dbURI); // Use to connect monogoDB with URI
         console.log('MongoDB connected successfully!');
     } catch (err) {
         console.error('MongoDB connection error:', err);
